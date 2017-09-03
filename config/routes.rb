8e798e806c4b724eb68f96bc9b257113c6c06874
Zorik Cherfas <zorik.cherfas@gmail.com>
2017-09-03 20:00:27 +0300

    View profile page

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
              controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 
+
+   resources :users, only: [:show]
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
