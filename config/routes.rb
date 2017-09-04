4e530df7e04de97ad582d879268617da48bac4dd
Zorik Cherfas <zorik.cherfas@gmail.com>
2017-09-04 19:39:35 +0300

    Room form controll part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
+  get 'pages/home'
   devise_for :users,
              path: '',
              path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
@@ -8,6 +8,16 @@ Rails.application.routes.draw do
 
 
    resources :users, only: [:show]
+   resources :rooms, except: [:edit] do
+     member do
+       get 'listing'
+       get 'pricing'
+       get 'description'
+       get 'photo_upload'
+       get 'amenities'
+       get 'location'
+     end
+   end
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
